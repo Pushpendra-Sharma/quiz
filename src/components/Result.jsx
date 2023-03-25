17172d269950ef92e4be3e1f6ebb074938969047
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-03-25 11:44:58 +0530

    style: fixed vertical height

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -12,10 +12,10 @@ const Result = ({ handleNav, data, time }) => {
   else message = PERFORMANCE.below;
 
   return (
-    <div className='w-full h-full sm:px-4 md:px-8 lg:px-20 xl:px-24 bg-purple-200'>
+    <div className='w-full sm:px-4 md:px-8 lg:px-20 xl:px-24 bg-purple-200'>
       <div className='flex flex-col justify-between gap-4 h-full sm:mx-4 md:mx-8 lg:mx-20 xl:mx-24'>
         <div className='flex flex-col gap-4 p-4'>
-          <p className='text-2xl text-gray-700 font-bold'>
+          <p className='text-2xl text-gray-700 font-bold text-center'>
             The Daily JavaScript Quiz
           </p>
           <div className='flex flex-col gap-2 items-center py-4 px-8 bg-white shadow-md rounded-xl'>
--- src/containers/AnswersContainer.jsx
+++ src/containers/AnswersContainer.jsx
@@ -1,23 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 import { Answer, Button } from '../components';
 import { questions } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { clearResponses } from '../redux/features/quiz/quizSlice';
 
 const AnswersContainer = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const questionArray = Object.values(questions);
 
+  const handleNav = () => {
+    dispatch(clearResponses());
+    navigate('/');
+  };
+
   return (
-    <div className='w-full h-full sm:px-4 md:px-8 lg:px-20 xl:px-24 bg-gray-200'>
+    <div className='w-full sm:px-4 md:px-8 lg:px-20 xl:px-24 bg-gray-200'>
       <div className='flex flex-col justify-between gap-4 h-full sm:mx-4 md:mx-8 lg:mx-20 xl:mx-24'>
-        <h1 className='text-3xl text-purple-700 font-bold text-center py-4'>Answers</h1>
-        <div>
+        <h1 className='text-3xl text-purple-700 font-bold text-center py-4'>
+          Answers
+        </h1>
+        <div className=''>
           {questionArray.map(ques => {
             const { id, title, answer } = ques;
             return <Answer key={id} title={title} answer={answer} id={id} />;
           })}
         </div>
         <div className='text-center py-8 bg-white rounded-t-md mx-2'>
-          <Button label='Home' callback={() => navigate('/')} />
+          <Button label='Home' callback={handleNav} />
         </div>
       </div>
     </div>
